b311ef69a20afc5f7a8fa03538eb902290792cef
Will Beason <willbeason@gmail.com>
2021-08-15 00:44:21 -0500

    get protos working as expected

--- pkg/classify/classify.pb.go
+++ pkg/classify/classify.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // 	protoc-gen-go v1.26.0
 // 	protoc        v3.17.3
-// source: pkg/classify/classified-articles.proto
+// source: pkg/classify/classify.proto
 
 package classify
 
@@ -31,7 +31,7 @@ type ClassifiedArticles struct {
 func (x *ClassifiedArticles) Reset() {
 	*x = ClassifiedArticles{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_pkg_classify_classified_articles_proto_msgTypes[0]
+		mi := &file_pkg_classify_classify_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -44,7 +44,7 @@ func (x *ClassifiedArticles) String() string {
 func (*ClassifiedArticles) ProtoMessage() {}
 
 func (x *ClassifiedArticles) ProtoReflect() protoreflect.Message {
-	mi := &file_pkg_classify_classified_articles_proto_msgTypes[0]
+	mi := &file_pkg_classify_classify_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -57,7 +57,7 @@ func (x *ClassifiedArticles) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use ClassifiedArticles.ProtoReflect.Descriptor instead.
 func (*ClassifiedArticles) Descriptor() ([]byte, []int) {
-	return file_pkg_classify_classified_articles_proto_rawDescGZIP(), []int{0}
+	return file_pkg_classify_classify_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *ClassifiedArticles) GetArticles() []*ClassifiedArticle {
@@ -79,7 +79,7 @@ type ClassifiedArticle struct {
 func (x *ClassifiedArticle) Reset() {
 	*x = ClassifiedArticle{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_pkg_classify_classified_articles_proto_msgTypes[1]
+		mi := &file_pkg_classify_classify_proto_msgTypes[1]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -92,7 +92,7 @@ func (x *ClassifiedArticle) String() string {
 func (*ClassifiedArticle) ProtoMessage() {}
 
 func (x *ClassifiedArticle) ProtoReflect() protoreflect.Message {
-	mi := &file_pkg_classify_classified_articles_proto_msgTypes[1]
+	mi := &file_pkg_classify_classify_proto_msgTypes[1]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -105,7 +105,7 @@ func (x *ClassifiedArticle) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use ClassifiedArticle.ProtoReflect.Descriptor instead.
 func (*ClassifiedArticle) Descriptor() ([]byte, []int) {
-	return file_pkg_classify_classified_articles_proto_rawDescGZIP(), []int{1}
+	return file_pkg_classify_classify_proto_rawDescGZIP(), []int{1}
 }
 
 func (x *ClassifiedArticle) GetId() int32 {
@@ -122,45 +122,42 @@ func (x *ClassifiedArticle) GetClassification() int32 {
 	return 0
 }
 
-var File_pkg_classify_classified_articles_proto protoreflect.FileDescriptor
+var File_pkg_classify_classify_proto protoreflect.FileDescriptor
 
-var file_pkg_classify_classified_articles_proto_rawDesc = []byte{
-	0x0a, 0x26, 0x70, 0x6b, 0x67, 0x2f, 0x63, 0x6c, 0x61, 0x73, 0x73, 0x69, 0x66, 0x79, 0x2f, 0x63,
-	0x6c, 0x61, 0x73, 0x73, 0x69, 0x66, 0x69, 0x65, 0x64, 0x2d, 0x61, 0x72, 0x74, 0x69, 0x63, 0x6c,
-	0x65, 0x73, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x22, 0x44, 0x0a, 0x12, 0x43, 0x6c, 0x61, 0x73,
-	0x73, 0x69, 0x66, 0x69, 0x65, 0x64, 0x41, 0x72, 0x74, 0x69, 0x63, 0x6c, 0x65, 0x73, 0x12, 0x2e,
-	0x0a, 0x08, 0x61, 0x72, 0x74, 0x69, 0x63, 0x6c, 0x65, 0x73, 0x18, 0x01, 0x20, 0x03, 0x28, 0x0b,
-	0x32, 0x12, 0x2e, 0x43, 0x6c, 0x61, 0x73, 0x73, 0x69, 0x66, 0x69, 0x65, 0x64, 0x41, 0x72, 0x74,
-	0x69, 0x63, 0x6c, 0x65, 0x52, 0x08, 0x61, 0x72, 0x74, 0x69, 0x63, 0x6c, 0x65, 0x73, 0x22, 0x4b,
-	0x0a, 0x11, 0x43, 0x6c, 0x61, 0x73, 0x73, 0x69, 0x66, 0x69, 0x65, 0x64, 0x41, 0x72, 0x74, 0x69,
-	0x63, 0x6c, 0x65, 0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x05, 0x52,
-	0x02, 0x69, 0x64, 0x12, 0x26, 0x0a, 0x0e, 0x63, 0x6c, 0x61, 0x73, 0x73, 0x69, 0x66, 0x69, 0x63,
-	0x61, 0x74, 0x69, 0x6f, 0x6e, 0x18, 0x02, 0x20, 0x01, 0x28, 0x05, 0x52, 0x0e, 0x63, 0x6c, 0x61,
-	0x73, 0x73, 0x69, 0x66, 0x69, 0x63, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x42, 0x2e, 0x5a, 0x2c, 0x67,
-	0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x77, 0x69, 0x6c, 0x6c, 0x62, 0x65,
-	0x61, 0x73, 0x6f, 0x6e, 0x2f, 0x77, 0x69, 0x6b, 0x69, 0x70, 0x65, 0x64, 0x69, 0x61, 0x2f, 0x70,
-	0x6b, 0x67, 0x2f, 0x63, 0x6c, 0x61, 0x73, 0x73, 0x69, 0x66, 0x79, 0x62, 0x06, 0x70, 0x72, 0x6f,
-	0x74, 0x6f, 0x33,
+var file_pkg_classify_classify_proto_rawDesc = []byte{
+	0x0a, 0x1b, 0x70, 0x6b, 0x67, 0x2f, 0x63, 0x6c, 0x61, 0x73, 0x73, 0x69, 0x66, 0x79, 0x2f, 0x63,
+	0x6c, 0x61, 0x73, 0x73, 0x69, 0x66, 0x79, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x22, 0x44, 0x0a,
+	0x12, 0x43, 0x6c, 0x61, 0x73, 0x73, 0x69, 0x66, 0x69, 0x65, 0x64, 0x41, 0x72, 0x74, 0x69, 0x63,
+	0x6c, 0x65, 0x73, 0x12, 0x2e, 0x0a, 0x08, 0x61, 0x72, 0x74, 0x69, 0x63, 0x6c, 0x65, 0x73, 0x18,
+	0x01, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x12, 0x2e, 0x43, 0x6c, 0x61, 0x73, 0x73, 0x69, 0x66, 0x69,
+	0x65, 0x64, 0x41, 0x72, 0x74, 0x69, 0x63, 0x6c, 0x65, 0x52, 0x08, 0x61, 0x72, 0x74, 0x69, 0x63,
+	0x6c, 0x65, 0x73, 0x22, 0x4b, 0x0a, 0x11, 0x43, 0x6c, 0x61, 0x73, 0x73, 0x69, 0x66, 0x69, 0x65,
+	0x64, 0x41, 0x72, 0x74, 0x69, 0x63, 0x6c, 0x65, 0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01,
+	0x20, 0x01, 0x28, 0x05, 0x52, 0x02, 0x69, 0x64, 0x12, 0x26, 0x0a, 0x0e, 0x63, 0x6c, 0x61, 0x73,
+	0x73, 0x69, 0x66, 0x69, 0x63, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x18, 0x02, 0x20, 0x01, 0x28, 0x05,
+	0x52, 0x0e, 0x63, 0x6c, 0x61, 0x73, 0x73, 0x69, 0x66, 0x69, 0x63, 0x61, 0x74, 0x69, 0x6f, 0x6e,
+	0x42, 0x0e, 0x5a, 0x0c, 0x70, 0x6b, 0x67, 0x2f, 0x63, 0x6c, 0x61, 0x73, 0x73, 0x69, 0x66, 0x79,
+	0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
-	file_pkg_classify_classified_articles_proto_rawDescOnce sync.Once
-	file_pkg_classify_classified_articles_proto_rawDescData = file_pkg_classify_classified_articles_proto_rawDesc
+	file_pkg_classify_classify_proto_rawDescOnce sync.Once
+	file_pkg_classify_classify_proto_rawDescData = file_pkg_classify_classify_proto_rawDesc
 )
 
-func file_pkg_classify_classified_articles_proto_rawDescGZIP() []byte {
-	file_pkg_classify_classified_articles_proto_rawDescOnce.Do(func() {
-		file_pkg_classify_classified_articles_proto_rawDescData = protoimpl.X.CompressGZIP(file_pkg_classify_classified_articles_proto_rawDescData)
+func file_pkg_classify_classify_proto_rawDescGZIP() []byte {
+	file_pkg_classify_classify_proto_rawDescOnce.Do(func() {
+		file_pkg_classify_classify_proto_rawDescData = protoimpl.X.CompressGZIP(file_pkg_classify_classify_proto_rawDescData)
 	})
-	return file_pkg_classify_classified_articles_proto_rawDescData
+	return file_pkg_classify_classify_proto_rawDescData
 }
 
-var file_pkg_classify_classified_articles_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
-var file_pkg_classify_classified_articles_proto_goTypes = []interface{}{
+var file_pkg_classify_classify_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_pkg_classify_classify_proto_goTypes = []interface{}{
 	(*ClassifiedArticles)(nil), // 0: ClassifiedArticles
 	(*ClassifiedArticle)(nil),  // 1: ClassifiedArticle
 }
-var file_pkg_classify_classified_articles_proto_depIdxs = []int32{
+var file_pkg_classify_classify_proto_depIdxs = []int32{
 	1, // 0: ClassifiedArticles.articles:type_name -> ClassifiedArticle
 	1, // [1:1] is the sub-list for method output_type
 	1, // [1:1] is the sub-list for method input_type
@@ -169,13 +166,13 @@ var file_pkg_classify_classified_articles_proto_depIdxs = []int32{
 	0, // [0:1] is the sub-list for field type_name
 }
 
-func init() { file_pkg_classify_classified_articles_proto_init() }
-func file_pkg_classify_classified_articles_proto_init() {
-	if File_pkg_classify_classified_articles_proto != nil {
+func init() { file_pkg_classify_classify_proto_init() }
+func file_pkg_classify_classify_proto_init() {
+	if File_pkg_classify_classify_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_pkg_classify_classified_articles_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_pkg_classify_classify_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*ClassifiedArticles); i {
 			case 0:
 				return &v.state
@@ -187,7 +184,7 @@ func file_pkg_classify_classified_articles_proto_init() {
 				return nil
 			}
 		}
-		file_pkg_classify_classified_articles_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+		file_pkg_classify_classify_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*ClassifiedArticle); i {
 			case 0:
 				return &v.state
@@ -204,18 +201,18 @@ func file_pkg_classify_classified_articles_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_pkg_classify_classified_articles_proto_rawDesc,
+			RawDescriptor: file_pkg_classify_classify_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
-		GoTypes:           file_pkg_classify_classified_articles_proto_goTypes,
-		DependencyIndexes: file_pkg_classify_classified_articles_proto_depIdxs,
-		MessageInfos:      file_pkg_classify_classified_articles_proto_msgTypes,
+		GoTypes:           file_pkg_classify_classify_proto_goTypes,
+		DependencyIndexes: file_pkg_classify_classify_proto_depIdxs,
+		MessageInfos:      file_pkg_classify_classify_proto_msgTypes,
 	}.Build()
-	File_pkg_classify_classified_articles_proto = out.File
-	file_pkg_classify_classified_articles_proto_rawDesc = nil
-	file_pkg_classify_classified_articles_proto_goTypes = nil
-	file_pkg_classify_classified_articles_proto_depIdxs = nil
+	File_pkg_classify_classify_proto = out.File
+	file_pkg_classify_classify_proto_rawDesc = nil
+	file_pkg_classify_classify_proto_goTypes = nil
+	file_pkg_classify_classify_proto_depIdxs = nil
 }
