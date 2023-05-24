b2cead20bf0076e132b65c3fa96be7ffc87d5b98
Will Beason <willbeason@gmail.com>
2023-05-24 08:59:20 -0500

    Analyze in/out degree links by gender.

--- cmd/reference-network/reference-network.go
+++ cmd/reference-network/reference-network.go
@@ -7,9 +7,11 @@ import (
 	"github.com/willbeason/wikipedia/pkg/documents"
 	"github.com/willbeason/wikipedia/pkg/flags"
 	"github.com/willbeason/wikipedia/pkg/jobs"
+	"github.com/willbeason/wikipedia/pkg/nlp"
 	"github.com/willbeason/wikipedia/pkg/pages"
 	"os"
 	"regexp"
+	"strings"
 	"sync"
 )
 
@@ -52,6 +54,8 @@ func runCmd(cmd *cobra.Command, args []string) error {
 
 	idMap := make(map[string]uint32)
 	titleMap := make(map[uint32]string)
+	female := make(map[uint32]bool)
+	male := make(map[uint32]bool)
 	resultMtx := sync.Mutex{}
 
 	docs, err := source(ctx, cancel)
@@ -60,9 +64,17 @@ func runCmd(cmd *cobra.Command, args []string) error {
 	}
 
 	idMapWork := jobs.Reduce(jobs.WorkBuffer, docs, func(page *documents.Page) error {
+		gender := nlp.DetermineGender(page.Text)
+
 		resultMtx.Lock()
 		idMap[page.Title] = page.Id
 		titleMap[page.Id] = page.Title
+		switch gender {
+		case nlp.Female:
+			female[page.Id] = true
+		case nlp.Male:
+			male[page.Id] = true
+		}
 		resultMtx.Unlock()
 
 		return nil
@@ -70,13 +82,19 @@ func runCmd(cmd *cobra.Command, args []string) error {
 
 	runner := jobs.NewRunner()
 	idMapWg := runner.Run(ctx, cancel, idMapWork)
+	// Must fully wait for ID Map to be created and the Badger database closed before opening another connection.
 	idMapWg.Wait()
+
+	fmt.Println("Biographies:", len(idMap))
+	fmt.Println("Women:", len(female))
+	fmt.Println("Men:", len(male))
 
 	network := make(map[uint32][]uint32)
 	networkMtx := sync.Mutex{}
 
 	source2 := pages.StreamDB(inDB, parallel)
 	docs2, err := source2(ctx, cancel)
+
 	if err != nil {
 		return err
 	}
@@ -90,9 +108,17 @@ func runCmd(cmd *cobra.Command, args []string) error {
 		matches := linkRegex.FindAllString(page.Text, -1)
 		var tos []uint32
 		for _, match := range matches {
+			// Strip square brackets.
+			match = match[2 : len(match)-2]
+
+			// Only consider before vertical bar.
+			if idx := strings.Index(match, "|"); idx != -1 {
+				match = match[:idx]
+			}
+
 			to, foundTo := idMap[match]
 			if !foundTo {
-				return fmt.Errorf("did not add ID for %q", page.Title)
+				continue
 			}
 
 			tos = append(tos, to)
@@ -108,22 +134,48 @@ func runCmd(cmd *cobra.Command, args []string) error {
 	networkWg := runner.Run(ctx, cancel, networkWork)
 	networkWg.Wait()
 
-	fmt.Println(len(idMap))
 	fmt.Println("Nodes:", len(network))
 
 	totalEdges := 0
+	f2f := 0
+	f2m := 0
+	m2f := 0
+	m2m := 0
 	singletons := 0
 
-	for _, edges := range network {
+	for id, edges := range network {
 		totalEdges += len(edges)
+
+		for _, edge := range edges {
+			switch {
+			case female[id] && female[edge]:
+				f2f++
+			case female[id] && male[edge]:
+				f2m++
+			case male[id] && female[edge]:
+				m2f++
+			case male[id] && male[edge]:
+				m2m++
+			}
+		}
+
 		if len(edges) == 0 {
 			singletons++
 		}
 	}
+
 	fmt.Println("Edges", totalEdges)
 	fmt.Println("Singletons", singletons)
+	fmt.Println("Avg F2F", float64(f2f)/float64(len(female)))
+	fmt.Println("Avg F2M", float64(f2m)/float64(len(female)))
+	fmt.Println("Avg M2F", float64(m2f)/float64(len(male)))
+	fmt.Println("Avg M2M", float64(m2m)/float64(len(male)))
 
-	return ctx.Err()
+	if ctx.Err() != nil {
+		return context.Cause(ctx)
+	}
+
+	return nil
 }
 
 var linkRegex = regexp.MustCompile(`\[\[[^]]+]]`)
